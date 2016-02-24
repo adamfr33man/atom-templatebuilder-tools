af271e4aff941fb3bbddb9e57a9d9d2985421280
Adam Freeman <shavenyaknz@gmail.com>
2016-02-24 23:48:03 +1300

    Saving progress so far

---
+++ src/linters/minitemplateLinter.js
@@ -0,0 +1,17 @@
+'use babel';
+
+module.exports.lintText = (text, path) => {
+  const messages = [],
+      minitemplate = {
+        minitemplates: {}
+      };
+
+      messages.push({
+        type: 'Error',
+        text: 'Test Error',
+        range:[ [0, 0], [0, 1] ],
+        filePath: path
+      });
+
+  return messages;
+};
