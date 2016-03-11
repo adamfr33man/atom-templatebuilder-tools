794e8dba6cd5d225e6b26e676089aaa7da07abc9
Adam Freeman <shavenyaknz@gmail.com>
2016-03-11 21:24:41 +1300

    Got basic parse errors firing.

--- src/linters/linter-structure_xml.js
+++ src/linters/linter-structure_xml.js
@@ -6,7 +6,27 @@ module.exports.lintText = (text, path) => {
   // Parse the XML Doc and return any Errors
   console.log('Hello from the structure linter');
 
-  let doc = parser.parseFromString(text, "application/xml");
+  let doc = parser.parseFromString(text, "application/xml"),
+      err = doc.querySelector('parsererror div');
+
+  if(err) {
+    let msg = err.textContent.trim(),
+        parts = msg.match(/line (\d+) at column (\d+): (.*)/);
+
+        if(parts) {
+          let row = parseInt(parts[1]) - 1,
+              col = parseInt(parts[2]) - 1;
+
+            messages.push({
+              type: 'Error',
+              text: parts[3],
+              range:[ [row, col], [row, col] ],
+              filePath: path
+            });
+        }
+    // "error on line 4 at column 5: error parsing attribute name"
+
+  }
 
   console.log(doc);
 
